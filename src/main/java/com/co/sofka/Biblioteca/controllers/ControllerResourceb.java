85ed5b4dc11cea220c735cc583f756b5c3016640
saracarohdm <saracarohdm@gmail.com>
2021-08-05 13:57:13 -0500

    Se realizo el controller de resource

--- src/main/java/com/co/sofka/Biblioteca/controllers/ControllerResourceb.java
+++ src/main/java/com/co/sofka/Biblioteca/controllers/ControllerResourceb.java
@@ -1,4 +1,50 @@
 package com.co.sofka.Biblioteca.controllers;
 
+import com.co.sofka.Biblioteca.dto.ResourcebDTO;
+import com.co.sofka.Biblioteca.service.ServiceResourceb;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recursos")
 public class ControllerResourceb {
+    @Autowired
+    ServiceResourceb serviceResourceb;
+
+    @PostMapping("/crearRecurso")
+    public ResponseEntity<ResourcebDTO> create(@RequestBody ResourcebDTO resourcebDTO) {
+        return new ResponseEntity(serviceResourceb.crear(resourcebDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/buscarRecurso/{id}")
+    public ResponseEntity<ResourcebDTO> findbyId(@PathVariable("id") String id) {
+        return new ResponseEntity(serviceResourceb.obtenerPorId(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/todosRecurso")
+    public ResponseEntity<List<ResourcebDTO>> findAll() {
+        return new ResponseEntity(serviceResourceb.obtenerTodos(), HttpStatus.OK);
+    }
+    @PutMapping("/modificarRecurso")
+    public ResponseEntity<ResourcebDTO> update(@RequestBody ResourcebDTO resourcebDTO) {
+        if (resourcebDTO.getIdResource() != null) {
+            return new ResponseEntity(serviceResourceb.modificar(resourcebDTO), HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/borrarPrestamo/{id}")
+    public ResponseEntity delete(@PathVariable("id") String id) {
+        try {
+            serviceResourceb.borrar(id);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
 }
